1b6cf832f7bc37e488a0f6bd5723b7e00ac1401d
BukenyaAbdusalaam <bukenyaabdusalaam508@gmail.com>
2024-01-27 02:43:13 +0300

    lets call it a day

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class Agri_demo extends StatelessWidget {
       body: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Icon(
                 Icons.location_pin,
@@ -25,109 +26,122 @@ class Agri_demo extends StatelessWidget {
               ),
               Text(
                 'Getting Location....',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          Image.asset(
-            'assets/plant.png',
-            // height: 150.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  width: 48.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 160, 242, 163),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  width: 48.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 160, 242, 163),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                ),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
               ),
             ],
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Expanded(
                 child: Container(
-                  margin: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.all(25.0),
                   width: 48.0,
-                  height: 120.0,
+                  height: 140.0,
                   decoration: BoxDecoration(
                       color: Color.fromARGB(255, 160, 242, 163),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  width: 48.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 160, 242, 163),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  width: 48.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 160, 242, 163),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  width: 48.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 160, 242, 163),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Column(
-                children: [Icon(Icons.explore), Text('Explore')],
-              ),
-              Column(
-                children: [Icon(Icons.favorite), Text('Favorite')],
-              ),
-              Column(
-                children: [Icon(Icons.home), Text('Home')],
-              ),
-              Column(
-                children: [Icon(Icons.notifications), Text('Notification')],
-              ),
-              Column(
-                children: [Icon(Icons.settings), Text('Settings')],
               )
             ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 48.0,
+                  height: 124.0,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 160, 242, 163),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 48.0,
+                  height: 124.0,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 160, 242, 163),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 48.0,
+                  height: 124.0,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 160, 242, 163),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 48.0,
+                  height: 124.0,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 160, 242, 163),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 48.0,
+                  height: 124.0,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 160, 242, 163),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 48.0,
+                  height: 124.0,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 160, 242, 163),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            color: Color.fromARGB(255, 228, 243, 228),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Column(
+                  children: [Icon(Icons.explore), Text('Explore')],
+                ),
+                Column(
+                  children: [Icon(Icons.favorite), Text('Favorite')],
+                ),
+                Column(
+                  children: [Icon(Icons.home), Text('Home')],
+                ),
+                Column(
+                  children: [Icon(Icons.notifications), Text('Notification')],
+                ),
+                Column(
+                  children: [Icon(Icons.settings), Text('Settings')],
+                )
+              ],
+            ),
           )
         ],
       ),
